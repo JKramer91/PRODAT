85f71aac20b94eddf8230a12e36c15500a5e074b
lrtzndrsn <lana@itu.dk>
2024-09-18 15:58:47 +0200

    Add compstring method
    Co-Authored-By: kram@itu.dk

--- Assignment2/Expr/Parse.fs
+++ Assignment2/Expr/Parse.fs
@@ -6,29 +6,28 @@ module Parse
 open System
 open System.IO
 open System.Text
-open (*Microsoft.*)FSharp.Text
+open FSharp.Text
 open Absyn
 
 (* Plain parsing from a string, with poor error reporting *)
 
-let fromString (str : string) : expr =
+let fromString (str: string) : expr =
     let lexbuf = Lexing.LexBuffer<char>.FromString(str)
-    try 
-      ExprPar.Main ExprLex.Token lexbuf
-    with 
-      | exn -> let pos = lexbuf.EndPos 
-               failwithf "%s near line %d, column %d\n" 
-                  (exn.Message) (pos.Line+1) pos.Column
-             
+
+    try
+        ExprPar.Main ExprLex.Token lexbuf
+    with exn ->
+        let pos = lexbuf.EndPos
+        failwithf "%s near line %d, column %d\n" (exn.Message) (pos.Line + 1) pos.Column
+
 (* Parsing from a text file *)
 
-let fromFile (filename : string) =
+let fromFile (filename: string) =
     use reader = new StreamReader(filename)
     let lexbuf = Lexing.LexBuffer<char>.FromTextReader reader
-    try 
-      ExprPar.Main ExprLex.Token lexbuf
-    with 
-      | exn -> let pos = lexbuf.EndPos 
-               failwithf "%s in file %s near line %d, column %d\n" 
-                  (exn.Message) filename (pos.Line+1) pos.Column
 
+    try
+        ExprPar.Main ExprLex.Token lexbuf
+    with exn ->
+        let pos = lexbuf.EndPos
+        failwithf "%s in file %s near line %d, column %d\n" (exn.Message) filename (pos.Line + 1) pos.Column
