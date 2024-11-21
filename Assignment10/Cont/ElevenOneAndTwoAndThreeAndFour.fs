034204acb335e1faf673dfe15188d5ceafd94ba3
lrtzndrsn <lana@itu.dk>
2024-11-21 12:08:24 +0100

    Så er d4r serveret!

--- Assignment10/Cont/ElevenOneAndTwoAndThreeAndFour.fs
+++ Assignment10/Cont/ElevenOneAndTwoAndThreeAndFour.fs
@@ -14,12 +14,14 @@ let rec lenCont xs c =
     | [] -> c 0
     | x :: xr -> lenCont xr (fun y -> c (1 + y))
 
-
+printfn "%A" "\nTest 11.1 i \n"
 printfn "%A" (lenCont [ 2; 5; 7 ] id)
 
 printfn "%A" (lenCont [ 2; 5; 7 ] (printf "The answer is ’% d’ \n"))
 
 // ii
+
+printfn "%A" "\nTest 11.1 ii \n"
 printfn "%A" (lenCont [ 2; 5; 7 ] (fun v -> 2 * v))
 // What happens is that we double the result of the length function, since the continuation gets applied as the last step in the function.
 
@@ -29,7 +31,8 @@ let rec leni xs acc =
     | [] -> acc
     | x :: xr -> leni xr (1 + acc)
 
-printfn "%A" (leni [ 2; 5; 7 ] 0)
+printfn "\nTest 11.1 iii. Results: \n"
+printfn "Result of leni with list and acc = 0 : %A" (leni [ 2; 5; 7 ] 0)
 
 // leni uses an accumulator to store the result of the length function, which is then returned when the list is empty.
 // This is a more efficient way of calculating the length of a list, since it avoids the overhead of using a continuation.
@@ -52,21 +55,28 @@ let rec revCont xs c =
     | [] -> c []
     | x :: xr -> revCont xr (fun y -> c (y @ [ x ]))
 
-printfn "%A" (rev [ 2; 5; 7 ])
-printfn "%A" (revCont [ 2; 5; 7 ] id)
+
+printfn "\nTest 11.2 i. Results: \n"
+printfn "Result of rev: %A" (rev [ 2; 5; 7 ])
+printfn "Result of revCont: %A" (revCont [ 2; 5; 7 ] id)
 
 
 // ii
-printfn "%A" (revCont [ 2; 5; 7 ] (fun v -> v @ v))
-// It basically duplicates the length within itself, since continuation gets applied as last step, it has build up the reversed list, and then it concatenates with "itself".
+printfn "\nTest 11.2 ii (revCont). Results: \n"
+printfn "Result of revCont with different continuation function: %A" (revCont [ 2; 5; 7 ] (fun v -> v @ v))
+
+// It basically duplicates the length within itself,
+//since continuation gets applied as last step, it has build up the reversed list, and then it concatenates with "itself".
 
 // iii
+printfn "\nTest 11.2 iii. (revi) Results: \n"
+
 let rec revi xs acc =
     match xs with
     | [] -> acc
     | x :: xr -> revi xr (x :: acc)
 
-printfn "%A" (revi [ 2; 5; 7 ] [])
+printfn "Result of revi: %A" (revi [ 2; 5; 7 ] [])
 
 // 11.3
 
@@ -84,9 +94,14 @@ let rec prodCont xs c =
     | [] -> c 1
     | x :: xr -> prodCont xr (fun y -> c (x * y))
 
-printfn "%A" (prod [ 2; 5; 7 ])
-printfn "%A" (prodCont [ 2; 5; 7 ] id)
 
+printfn "\nTest 11.3 i. Results: \n"
+printfn "Result of prod: %A" (prod [ 2; 5; 7 ])
+printfn "Result of prodCont with id : %A" (prodCont [ 2; 5; 7 ] id)
+
+// 11.4
+
+//a
 let rec prodContOptimised xs c =
     match xs with
     | [] -> c 1
@@ -102,14 +117,28 @@ let rec prodContOptimised2 xs c =
     | x :: xr when x = 0 -> c 0
     | x :: xr -> prodContOptimised2 xr (fun y -> c (x * y))
 
+printfn "\nTest 11.4 a) Results: \n"
+printfn "prod Optimised for returning 0 when encountering 0 \n"
+printfn "result of prodContOptimised with id: %A" (prodContOptimised [ 0; 2; 5; 7 ] id)
 
-printfn "%A" "Optimised for returning 0 when encountering 0"
-printfn "%A" (prodContOptimised [ 0; 2; 5; 7 ] id)
-printfn "%A" (prodContOptimised [ 0; 2; 5; 7 ] (printf "The answer is ’% d’ \n"))
-printfn "%A" (prodContOptimised2 [ 0; 2; 5; 7 ] id)
-printfn "%A" (prodContOptimised2 [ 0; 2; 5; 7 ] (printf "The answer is ’% d’ \n"))
+printfn
+    "result of prodContOptimised with id with print-function %A"
+    (prodContOptimised [ 0; 2; 5; 7 ] (printf "The answer is ’% d’ \n"))
 
-let rec prodi xs acc = 
-    match xs with 
-    | [] -> 1 * acc 
-    | x :: xr -> 
+printfn "Result of prodContOptimised2 with id : %A" (prodContOptimised2 [ 0; 2; 5; 7 ] id)
+
+printfn
+    "Result of prodContOptimised2 with print function %A"
+    (prodContOptimised2 [ 0; 2; 5; 7 ] (printf "The answer is ’% d’ \n"))
+
+
+printfn "\nTest 11.4 b) (prodi) \n"
+
+let rec prodi xs acc =
+    match xs with
+    | [] -> 1 * acc
+    | x :: xr when x = 0 -> 0 * acc
+    | x :: xr -> prodi xr (x * acc)
+
+printfn "result of prodi without zero:  %A" (prodi [ 2; 5; 7 ] 1)
+printfn "result of prodi with zero in the list : %A" (prodi [ 2; 0; 5; 7 ] 1)
